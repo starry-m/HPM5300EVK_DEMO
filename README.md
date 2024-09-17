9c77dddf62a4b4b04699e0d2dcde4fd6b6e931f5
starry-m <2386279494@qq.com>
2024-09-18 04:14:22 +0800

    格式整理+文档初写

--- README.md
+++ README.md
@@ -25,3 +25,105 @@ IB,34
 修改`libraries\hpm_sdk\components\SConscript   path = [ os.path.join(cwd, 'debug_console'), os.path.join(cwd, 'touch'), os.path.join(cwd, 'usb'), os.path.join(cwd, 'dma_mgr'), os.path.join(cwd, 'adc')]
 `
 
+
+
+# 一、项目概述
+本项目为基于HPM5300 EVK开发板、无刷电机驱动板和2208无刷电机组成的FOC旋钮。使用FOC的高度可控性，模拟各种旋钮的转动效果。
+## 目标功能
+- 配合无刷电机驱动板，完成FOC闭环控制
+- 圆形屏幕跑LVGL，做图形交互
+- USB HID 接入，模拟实际设备
+  
+# 二、设计思路
+- 初期使用现成的开发板、驱动板和屏幕做功能验证，三大部分分别调试完成，连接完成功能展示
+- 做集成化设计，重新设计版图和结构，做成X-Knob这种形式的。USB 5V升压驱动电机，只需连接USB，不需要其他电源供电
+  
+## 三、实现过程
+
+### 1、硬件介绍
+
+- 硬件连接
+  
+|HPM5300 EVK 引脚|外接模块|说明|
+|-|-|-|
+|PA26|M0_IN1|FOC驱动板|
+|PA27|NC|-|
+|PA28|M0_IN2|-|
+|PA29|NC|-|
+|PA30|M0_IN3|-|
+|PA31|NC|-|
+|PB13|IA|-|
+|PB14|IB|-|
+|PB02|SCL|AS5600|
+|PB03|SDA|-|
+|PB05|SCK|GC9A01|
+|PB07|MOSI|-|
+|PB10|DC|-|
+
+- 磁编码器部分
+我所使用的无刷电机套装传感器使用的是磁编码器AS5600，并且给出的接口是I2C形式，因此需要使用开发板的I2C接口。  
+
+- 驱动板部分
+这里使用的是`灯哥开源`的V3P电机驱动板，这是一个两路无刷驱动板，本次只使用到了其中一路。
+- 屏幕部分
+这里使用的是1.28寸 GC9A01 240X240 SPI屏，CS已接下拉电阻，RST已接复位电路，背光无法控制，默认打开，因此这三个角不需要接。
+
+### 2、软件实现
+
+本工程使用的FOC算法部分，移植自[HPM5361MiniFOC](https://github.com/LX050724/HPM5361MiniFOC)
+，在此感谢开源。本工程基于`RT Thread studio` IDE创建和调试。
+
+- FOC算法适配
+参考工程项目使用HPM的SEI接口连接传感器，获取角度数据，并依靠PWM连接触发，完成环路计算（在终中断中实现硬实时）。但我这套硬件只能用I2C读取，但是因为使用了RTT 标准I2C驱动，读写中有加锁，无法在中断中使用，会直接断言报错。且原工程环路计算频率极高（50K），在RTOS中直接使用不现实，一是I2C跟不上，二是影响RTOS运行。再者，本项目的目标是做旋钮效果模拟，因此选择降低环路计算频率。
+下面主要讲下适配过程中的一些重要的地方。
+`1:`编码器型号更换，原始角度从16位变为12位。
+`2:`project_config中修改采样电阻和放大倍数。  
+`3:`PID系数重新调整。  
+`4:`环路计算改为定时100Hz运行一次。  
+但是因为计算频率的降低，导致使用电流环做反馈时一直碰到卡顿和震荡现象。尝试连续几天的PID调参，一直无法解决，遂选择放弃电流环，选择最简单的办法，使用传感器的角度直接做位置环路控制，进行计算变换。
+并且因为时间没安排好，以及工作太多，导致功能实现进度极慢，目前这部分还只完成了简单的多档旋钮模拟。
+- 界面LVGL适配
+屏幕的驱动参考自[此处仓库](https://github.com/liyanboy74/gc9a01-esp-idf)。  
+``` c
+static int LCD_Device_Init(void)
+{
+    lcd_device = RT_NULL;
+
+    lcd_device = (struct rt_spi_device *)rt_malloc(sizeof(struct rt_spi_device));
+    if(RT_NULL == lcd_device)
+    {
+        LOG_E("Failed to malloc the spi device.");
+        return -RT_ENOMEM;
+    }
+    if (RT_EOK != rt_spi_bus_attach_device_cspin(lcd_device, SPI_LCD_DEVICE_NAME, "spi3",rt_pin_get("PB04"), RT_NULL))
+    {
+        LOG_E("Failed to attach the spi device.");
+        return -RT_ERROR;
+    }
+
+    if (RT_NULL == rt_device_find(SPI_LCD_DEVICE_NAME))
+    {
+        LOG_E("Failed to probe the lcd.");
+        return -RT_ERROR;
+    }
+    //配置spi参数
+    struct rt_spi_configuration cfg;
+    {
+        cfg.data_width = 8;//数据宽度为八位
+        cfg.mode = RT_SPI_MASTER | RT_SPI_MODE_0 | RT_SPI_MSB;
+        cfg.max_hz = 80 * 1000 * 1000;//
+    }
+    rt_spi_configure(lcd_device, &cfg);
+    //前面的频率配置没用，锁在了80M。
+    rt_pin_mode(LCD_DC_PIN, PIN_MODE_OUTPUT);
+    GC9A01_Init();
+    GC9A01_FillRect(0,0,239,239,0XF800);
+    return RT_EOK;
+}
+/* 导出到自动初始化 */
+INIT_DEVICE_EXPORT(LCD_Device_Init);
+```
+随后是LVGL接口的适配，
+- USB HID适配
+
+
--- applications/main.c
+++ applications/main.c
@@ -55,17 +55,12 @@ typedef struct
 volatile int write_ptr;
 DMA_ATTR just_float_data vofa_data[2];
 
-/* 串口设备句柄 */
-static rt_device_t serial;
-#define uart_name       "uart2"
-
-
 void tick_ms_isr(void)
 {
     if (gptmr_check_status(APP_BOARD_GPTMR, GPTMR_CH_RLD_STAT_MASK(APP_BOARD_GPTMR_CH))) {
         gptmr_clear_status(APP_BOARD_GPTMR, GPTMR_CH_RLD_STAT_MASK(APP_BOARD_GPTMR_CH));
 //        board_led_toggle();
-        //10 KHz
+        //100 Hz
         mt6701_isr_callback(0);
 
     }
@@ -110,66 +105,9 @@ static void mt6701_isr_callback(uint32_t isr_flag)
 ////        board_led_write(BOARD_LED_GPIO_RED_PIN, BOARD_LED_ON_LEVEL);
 //        rt_kprintf( "VBUS Error: %dmV\n", (int)(VBUS * 1000));
 //    }
-
     motor0.bus_voltage = 12.0;//VBUS
 
     Motor_RunFoc(&motor0);
-
-    /* 使能DTR才发送数据，方便vofa静止查看波形 */
-    if (0)
-    {
-        write_ptr=0;
-        vofa_data[write_ptr].data[0] = motor0.qd_current.iq;
-        vofa_data[write_ptr].data[1] = motor0.qd_current_exp.iq;
-        vofa_data[write_ptr].data[2] = motor0.qd_current.id;
-        vofa_data[write_ptr].data[3] = motor0.qd_current_exp.id;
-        vofa_data[write_ptr].data[4] = motor0.speed;
-        vofa_data[write_ptr].data[5] = motor0.speed_exp;
-
-        // vofa_data[write_ptr].data[2] = motor0.speed_pll.theta;
-        // vofa_data[write_ptr].data[3] =
-        //     motor0.speed_pll.speed / motor0.encoder.pole_pairs / (2 * F_PI) * 60 * SPEED_PID_FREQUENCY;
-        // vofa_data[write_ptr].data[5] = foc_para.currentdpipar.outval;
-
-        vofa_data[write_ptr].data[6] = motor0.uvw_current.iu;
-        vofa_data[write_ptr].data[7] = motor0.uvw_current.iv;
-        vofa_data[write_ptr].data[8] = motor0.uvw_current.iw;
-
-        vofa_data[write_ptr].data[9] = motor0.raw_angle;
-
-        vofa_data[write_ptr].data[10] = VBUS;
-        vofa_data[write_ptr].data[11] = motor0.power;
-
-        // vofa_data[write_ptr].data[14] = pll_speed * PWM_FREQUENCY * 60 / 2 / F_PI;
-
-//         vofa_data[write_ptr].data[12] = motor0.pwm.pwm_u;
-//         vofa_data[write_ptr].data[13] = motor0.pwm.pwm_v;
-//         vofa_data[write_ptr].data[14] = motor0.pwm.pwm_w;
-
-        // vofa_data[write_ptr].data[14] = (float)hpm_csr_get_core_cycle() / hpm_core_clock;
-
-        rt_device_write(serial, 0, &vofa_data[write_ptr], (sizeof(just_float_data) - 1));
-//        if (write_ptr == 0)
-//        {
-//            if (!ep_tx_busy_flag)
-//            {
-//                ep_tx_busy_flag = true;
-//                usbd_ep_start_write(0x81, &vofa_data[BUF_NUM / 2], sizeof(just_float_data) * BUF_NUM / 2);
-//            }
-//        }
-//
-//        if (write_ptr == BUF_NUM / 2)
-//        {
-//            if (!ep_tx_busy_flag)
-//            {
-//                ep_tx_busy_flag = true;
-//                usbd_ep_start_write(0x81, &vofa_data[0], sizeof(just_float_data) * BUF_NUM / 2);
-//            }
-//        }
-        write_ptr = (write_ptr + 1) % 2;
-    }
-
-    // gpio_write_pin(HPM_GPIO0, GPIO_OE_GPIOB, 5, 0);
 }
 void motor0_get_uvw_current(MotorClass_t *motor, foc_uvw_current_t *uvw_current)
 {
@@ -191,55 +129,9 @@ void motor0_enable_pwm(MotorClass_t *motor, bool en)
 {
     en ? pwm_enable_all_output() : pwm_disable_all_output();
 }
-
-/*
- *     {"exp_iq", NULL, &motor0.qd_current_exp.iq},
-    {"exp_id", NULL, &motor0.qd_current_exp.id},
-    {"id_p", NULL, &motor0.current_id_pid.kp},
-    {"id_i", NULL, &motor0.current_id_pid.ki},
-    {"id_il", NULL, &motor0.current_id_pid.integral_limit},
-    {"iq_p", NULL, &motor0.current_iq_pid.kp},
-    {"iq_i", NULL, &motor0.current_iq_pid.ki},
-    {"iq_il", NULL, &motor0.current_iq_pid.integral_limit},
-
-    {"speed_p", NULL, &motor0.speed_pid.kp},
-    {"speed_i", NULL, &motor0.speed_pid.ki},
-    {"exp_speed", NULL, &motor0.speed_exp},
-
-    {"ang_p", NULL, &motor0.angle_pid.kp},
-    {"ang_i", NULL, &motor0.angle_pid.ki},
-    {"exp_ang", set_expang, NULL},
-    {"speed_filter", NULL, &motor0.speed_pll.pi.kp},
- * */
-/* 接收数据回调函数 */
-static rt_err_t uart_input(rt_device_t dev, rt_size_t size)
-{
-    return RT_EOK;
-}
 int main(void)
 {
     app_init_led_pins();
-    __attribute__((section(".noncacheable.init"), aligned(4))) static char str[] = "hello RT-Thread!\r\n";
-    /* 查找串口设备 */
-       serial = rt_device_find(uart_name);
-       if (!serial)
-       {
-           rt_kprintf("find %s failed!\n", uart_name);
-           return RT_ERROR;
-       }
-       /* 以 硬件FIFO超时接收及轮询发送方式打开串口设备 */
-       rt_device_open(serial, RT_DEVICE_FLAG_RX_BLOCKING | RT_DEVICE_FLAG_TX_BLOCKING);
-       /* 设置接收回调函数 */
-       rt_device_set_rx_indicate(serial, uart_input);
-       /* 发送字符串 */
-//       rt_device_write(serial, 0, str, (sizeof(str) - 1));
-       for (int i = 0; i < 2; i++)
-       {
-           vofa_data[i].tail[0] = 0x00;
-           vofa_data[i].tail[1] = 0x00;
-           vofa_data[i].tail[2] = 0x80;
-           vofa_data[i].tail[3] = 0x7f;
-       }
     /* PLL测速参数 */
        // motor0.speed_pll.pi.kp = 0.2;
        // motor0.speed_pll.pi.ki = 0.1;
@@ -249,7 +141,7 @@ int main(void)
     motor0.speed_pll.pi.output_limit = 200;
 
     */
-    motor0.angle_pid.kp = 0.001f;
+    motor0.angle_pid.kp = 0.0015f;
     motor0.angle_pid.ki = 0.00f;
     motor0.angle_pid.integral_limit = 100;
     motor0.angle_pid.output_limit = 100;
@@ -311,7 +203,6 @@ int main(void)
        rt_kprintf("设置电机工作模式\r\n");
        Motor_SetMode(&motor0, ANGLE_MODE);
        //Motor_SetMode(&motor0, CURRENT_MODE);
-//           encoder_set_callback(mt6701_isr_callback);
    }
 //   init_gptmr_pins(APP_BOARD_GPTMR);
    rt_kprintf("gptmr timer basic test\n");
@@ -320,28 +211,16 @@ int main(void)
    uint16_t last_angle = 0;
    int16_t angle_diff = 0;
     while(1){
-           count++;
-           read_raw_angel=encoder_get_rawAngle();
+       count++;
+       read_raw_angel=encoder_get_rawAngle();
 
-           motor0.angle_exp = ((int)((read_raw_angel + 256)/512))*512;
-//           angle_diff = read_raw_angel - last_angle;
-//           if(angle_diff>2048)
-//               angle_diff-=4096;
-//           if(angle_diff<-2048)
-//              angle_diff+=4096;
-//           motor0.speed = angle_diff/4096.0f*2.0f*F_PI;
-//           last_angle = read_raw_angel;
-           //motor->speed = foc_pid_diff(motor->raw_angle, last_angle, 4096)
-//           app_led_write(0, APP_LED_ON);
-           rt_thread_mdelay(1);
-           if(count %50==0)
-           {
-               board_led_toggle();
-               //rt_kprintf("speed:%d\r\n",(int)( motor0.speed*100000));
-               //rt_kprintf("[current]q=%d.%d,d=%d.%d\r\n",(int)motor0.qd_current.iq,((int)(motor0.qd_current.iq*100)%100),(int)motor0.qd_current.id,((int)(motor0.qd_current.id*100)%100));
-               //   rt_kprintf("current:%d,%d,%d\r\n",(int)(motor0.qd_current.iq*1000),(int)(motor0.qd_current_exp.iq*1000),(int)(motor0.qd_voltage_exp.iq*1000));
+       motor0.angle_exp = ((int)((read_raw_angel + 256)/512))*512;
 
-           }
+       rt_thread_mdelay(1);
+       if(count %50==0)
+       {
+           board_led_toggle();
+       }
 //           app_led_write(0, APP_LED_OFF);
 //           rt_thread_mdelay(500);
        }
